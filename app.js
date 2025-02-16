4248259cc354f2a5d8439c4e5a8be8e9210b49fc
unknown <zignale92@gmail.com>
2025-02-16 19:52:21 +0100

    js faq start

---
+++ app.js
@@ -0,0 +1,16 @@
+// FAQ accordian
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const faqContainer = document.querySelectorAll(".faq-content");
+
+    faqContainer.addEventListener("click", ()=>{
+        alert(1);
+    })
+});
+
+
+
+
+
+
+
